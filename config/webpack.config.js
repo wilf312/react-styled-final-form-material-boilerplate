64d10eaefbf7e69fe8b1f0dd0870e5aec12e7ef8
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-02-13 00:00:48 +0900

    temporary delete

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -40,10 +40,6 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'),
       IS_DEVELOPMETN: true
-    }),
-    new HtmlWebpackPlugin({
-      template: src + '/index.html',
-      filename: 'index.html'
     })
   ],
 
